227ff2e2a9dde06a7fea73cfc0d98612d8493838
Ole-Martin <ole-ma@outlook.com>
2018-03-13 13:15:26 +0100

    Suggestion for sprite drawing

--- src/sample/Wall.java
+++ src/sample/Wall.java
@@ -5,4 +5,8 @@ public class Wall extends GameObject{
     public Wall (double x, double y, int height, int width){
         super(x, y, height, width);
     }
+
+    public void drawWall(){
+        
+    }
 }
