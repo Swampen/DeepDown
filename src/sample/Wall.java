39b076d98f5d4d6d8360e6032d77c33fe3d0a3e7
MichaelThoresen <32193552+MichaelThoresen@users.noreply.github.com>
2018-03-18 18:25:41 +0100

    Removed unnessesary code

--- src/sample/Wall.java
+++ src/sample/Wall.java
@@ -6,16 +6,10 @@ import javafx.scene.image.Image;
 
 public class Wall extends GameObject{
 
-    private Image image;
-    private double width;
-    private double height;
-    private double xPos;
-    private double yPos;
-
     public Wall (double x, double y, int height, int width){
         super(x, y, height, width);
     }
 
-    
+
 
 }
