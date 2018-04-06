f444cf93bcab5d5fc2fae59b468cfbb58d8a7f88
Ole-Martin <ole-ma@outlook.com>
2018-04-06 11:10:32 +0200

    Playermovement improvement

--- src/sample/Level1Controller.java
+++ src/sample/Level1Controller.java
@@ -1,28 +1,21 @@
 package sample;
 
-import com.sun.jdi.LongValue;
-import com.sun.jdi.Type;
-import com.sun.jdi.VirtualMachine;
 import javafx.animation.AnimationTimer;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class Level1Controller {
@@ -32,6 +25,9 @@ public class Level1Controller {
     @FXML
     protected AnchorPane anchor;
     protected GraphicsContext gc;
+    @FXML
+    protected Label scoreLabel;
+    private int score = 2000;
 
 
 
@@ -48,12 +44,7 @@ public class Level1Controller {
         ArrayList<Sprite> sprites;
         sprites = level1.drawBoard(gc);
         image = new Image(new FileInputStream("src/sample/DeepDownTileSet.png"));
-        /*try {
-            sprites = level1.drawBoard(gc);
-            image = new Image(new FileInputStream("src/sample/DeepDownTileSet.png"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+
         Avatar player = new Avatar(1*40, 16*40, 40, 40, 3, true, 0, 0);
         gc.drawImage(image, 80, 0, 40, 40, player.getX(),player.getY(),40,40);
 
@@ -77,11 +68,6 @@ public class Level1Controller {
                 }if (e.getCode() == KeyCode.RIGHT){
                     rightPressed.set(true);
                 }
-
-                /*String key = e.getCode().toString();
-                if(!input.contains(key)){
-                    input.add(key);
-                }*/
             }
         });
 
@@ -97,8 +83,6 @@ public class Level1Controller {
                 }if (e.getCode() == KeyCode.RIGHT){
                     rightPressed.set(false);
                 }
-                /*String key = e.getCode().toString();
-                input.remove(key);*/
             }
         });
 
@@ -106,38 +90,38 @@ public class Level1Controller {
             public void handle(long currentTime){
                 double t = (currentTime - startTime ) / 1000000000.0;
 
+                int currentScore = score-(int)t;
+
+                scoreLabel.setText(Integer.toString(currentScore));
+
+                boolean canMove = true;
+
                 Sprite playerSprite = new Sprite(finalImage, player);
                 for (int i = 0; i < sprites.size(); i++) {
                     if(playerSprite.collision(sprites.get(i))){
-                        double currentXVelo = player.getXVelo();
-                        double currentYVelo = player.getYVelo();
-                        System.out.println(playerSprite.getBoundary());
+                        canMove = false;
+                        break;
+
+                        //System.out.println(playerSprite.getBoundary());
                     }
                 }
 
 
-                player.setVelo(0,0);
-                if (upPressed.getValue()){
-                    player.setVelo(0,-3);
-                }if (downPressed.getValue()){
-                    player.setVelo(0,3);
-                }if (leftPressed.getValue()){
-                    player.setVelo(-3,0);
-                }if (rightPressed.getValue()){
-                    player.setVelo(3,0);
-                }
-
-                /*if (input.contains("UP")){
-                    player.setVelo(0,-3);
-                }if (input.contains("DOWN")){
-                    player.setVelo(0,3);
-                }if (input.contains("RIGHT")){
-                    player.setVelo(3,0);
-                } if (input.contains("LEFT")){
-                    player.setVelo(-3,0);
-                }*/
-
-
+                    player.setVelo(0, 0);
+                //if (canMove) {
+                    if (upPressed.getValue()) {
+                        player.setyVelo(-3);
+                    }
+                    if (downPressed.getValue()) {
+                        player.setyVelo(3);
+                    }
+                    if (leftPressed.getValue()) {
+                        player.setxVelo(-3);
+                    }
+                    if (rightPressed.getValue()) {
+                        player.setxVelo(3);
+                    }
+                //}
                 player.posUpdate();
 
                 gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -147,7 +131,6 @@ public class Level1Controller {
                 }catch (Exception e){
                     e.printStackTrace();
                 }
-                //System.out.println("x: " + player.getX() + ", y: " + player.getY() + input);
                 playerSprite.render(gc, 80, 0);
 
 
