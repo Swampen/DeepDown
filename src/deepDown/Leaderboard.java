9cf231f6c33d814f87c59348dd6a49536ce40389
MichaelThoresen <mimothor@hotmail.com>
2018-04-27 09:23:01 +0200

    Added name to leaderboard

--- src/deepDown/Leaderboard.java
+++ src/deepDown/Leaderboard.java
@@ -8,22 +8,23 @@ public class Leaderboard {
     private String highScores;
 
     private ArrayList<Integer> topScores;
+    private ArrayList<String> topName;
 
     public Leaderboard() {
         filePath = new File("Files").getAbsolutePath();
-        highScores = "Scores";
+        highScores = "scores";
 
         topScores = new ArrayList<Integer>();
-        //topName = new ArrayList<String>();
+        topName = new ArrayList<String>();
     }
 
-    public void addScore(int score/*, String name */){
+    public void addScore(int score, String name){
         for(int i = 0; i < topScores.size(); i++) {
             if(score >= topScores.get(i)){
                 topScores.add(i, score);
                 topScores.remove(topScores.size()-1);
-                //topName.add(i , name);
-                //topName.remove(topName.size()-1);
+                topName.add(i , name);
+                topName.remove(topName.size()-1);
                 return;
             }
         }
@@ -41,10 +42,11 @@ public class Leaderboard {
             topScores.clear();
 
             String[] scores = reader.readLine().split("-");
+            String[] names = reader.readLine().split("-");
 
             for (int i =0; i < scores.length; i++){
                 topScores.add(Integer.parseInt(scores[i]));
-                //topName.add(name)
+                topName.add(names[i]);
             }
 
             reader.close();
@@ -61,8 +63,8 @@ public class Leaderboard {
             BufferedWriter writer = new BufferedWriter(output);
 
             writer.write(topScores.get(0) + "-" + topScores.get(1) + "-" + topScores.get(2) + "-" + topScores.get(3) + "-" + topScores.get(4));
-            //write.newLine();
-            //writer.write(topName.get(0) + "-" + topName.get(1) + "-" + topName.get(2) + "-" + topName.get(3) + "-" + topName.get(4));
+            writer.newLine();
+            writer.write(topName.get(0) + "-" + topName.get(1) + "-" + topName.get(2) + "-" + topName.get(3) + "-" + topName.get(4));
             writer.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -78,7 +80,7 @@ public class Leaderboard {
 
             writer.write("0-0-0-0-0");
             writer.newLine();
-            writer.write("0-0-0-0-0");
+            writer.write(".....-.....-.....-.....-.....");
             writer.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -88,5 +90,9 @@ public class Leaderboard {
 
     public int getHighScore(int i) {
         return topScores.get(i);
+    }
+
+    public String getNames(int i) {
+        return topName.get(i);
     }
 }
