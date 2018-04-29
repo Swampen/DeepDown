ec5a13d372d88da6b31d236102053b061cd8c0a5
MichaelThoresen <32193552+MichaelThoresen@users.noreply.github.com>
2018-04-29 12:12:26 +0200

    Made Leaderboards always fill the screen and added an error when you try to load a save file that doesn't exist

--- src/deepDown/controllers/LeaderboardController.java
+++ src/deepDown/controllers/LeaderboardController.java
@@ -35,18 +35,14 @@ public class LeaderboardController {
         fillScores(nr3, 3, lb.getNames(2).length(), Integer.toString(lb.getHighScore(2)).length(), lb.getNames(2), Integer.toString(lb.getHighScore(2)));
         fillScores(nr4, 4, lb.getNames(3).length(), Integer.toString(lb.getHighScore(3)).length(), lb.getNames(3), Integer.toString(lb.getHighScore(3)));
         fillScores(nr5, 5, lb.getNames(4).length(), Integer.toString(lb.getHighScore(4)).length(), lb.getNames(4), Integer.toString(lb.getHighScore(4)));
-        System.out.println(nr1.getLayoutBounds().getWidth());
-        System.out.println(nr2.getLayoutBounds().getWidth());
     }
 
     public void fillScores(Text nr, int x, int nLength, int sLength, String name, String score) {
         String dot = ".";
-        for(int i = 0; i < 30; i++){
-            if(nLength + sLength == i){
-                for(int j=30; j>i; j--){
-                    dot += "." ;
-                    System.out.println(dot.length());
-                }
+        for(double i = 0; i < 80; i++){
+            if(nr.getLayoutBounds().getWidth() < (1280-15.39)){
+                dot += "." ;
+                System.out.println(dot.length());
                 nr.setText(x +". " + name + dot + score);
             }
         }
